6d1418f12d2442c8493542284ecc85c5d59442a9
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-03 19:00:49 +0200

    add random number

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,6 +1,10 @@
 // Gioco dei dadi
 // 1. Generaro un numero random da 1 a 6 sia per il giocatore sia per il computer
+const userNumber = Math.floor(Math.random() * 6) + 1;
+console.log(userNumber);
 
+const pcNumber = Math.floor(Math.random() * 6) + 1;
+console.log(pcNumber);
 
 // 2. Stabilisco il vincitore in base a chi fa il punteggio più alto
 
