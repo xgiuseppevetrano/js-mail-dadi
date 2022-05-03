54010c21dace5ce3d66e3e7fdf5d79585e2d4767
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-03 15:19:15 +0200

    first commit

---
+++ dadi/js/script.js
@@ -0,0 +1,6 @@
+// Gioco dei dadi
+// 1. Generaro un numero random da 1 a 6 sia per il giocatore sia per il computer
+
+
+// 2. Stabilisco il vincitore in base a chi fa il punteggio più alto
+
---
+++ mail/js/script.js
@@ -0,0 +1,9 @@
+// Mail
+// 1. Chiedo all’utente la sua email
+
+
+// 2. Controllo che la mail sia nella lista di chi può accedere
+
+
+// 3. Stampo un messaggio appropriato sull’esito del controllo
+
