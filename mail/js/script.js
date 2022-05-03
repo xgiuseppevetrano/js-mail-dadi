44c118e978fc0f33e5d735476b11cf22f0021644
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-03 18:21:38 +0200

    add mail controll

--- mail/js/script.js
+++ mail/js/script.js
@@ -3,7 +3,20 @@
 const mail = prompt("Inserisci la tua mail");
 
 // 2. Controllo che la mail sia nella lista di chi può accedere
+const usersMail = ["giovanni.rossi@gmail.com", "gianni.rossi@gmail.com", "filippo.rossi@gmail.com", "giorgio.rossi@gmail.com"];
+let isValidMail = false;
 
+for (let i = 0; i < usersMail.length; i++) {
+    if (mail === usersMail[i]) {
+        isValidMail = true;
+    }
+}
 
 // 3. Stampo un messaggio appropriato sull’esito del controllo
-
+if (isValidMail) {
+    console.log("Mail valida, puoi accedere!");
+    alert("Puoi accedere!");
+} else {
+    console.log("Mail non valida, non puoi accedere!");
+    alert("Non puoi accedere!");
+}
